778632d31bb72dee92c6b81b4f247073d211b88b
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2025-07-31 22:25:49 +0100

    po

--- backed/Routes/doctorRoutes.js
+++ backed/Routes/doctorRoutes.js
@@ -14,6 +14,6 @@ doctorRoute.get("/allAppointments",authAdmin,allAppointments);
 doctorRoute.post("/cancelByAdmin",authAdmin,canelAppointment);
 doctorRoute.get("/dashboard",authAdmin,dashboardData);
 doctorRoute.post("/drLogin",doctorLogin);
-doctorRoute.get("/getDoctorAppointments",authDoctor,getDoctorAppointments);
+doctorRoute.post("/getDoctorAppointments",authDoctor,getDoctorAppointments);
 
 module.exports=doctorRoute;
